aa1d7464a1c07c02b5dfd1cd35673981cc444b90
sutanuadhikary <sutanu.adhikary.8131@gmail.com>
2024-04-02 04:26:06 +0530

    minor changes

--- Mainpage/src/routes/MyProfile/My-profile.tsx
+++ Mainpage/src/routes/MyProfile/My-profile.tsx
@@ -3,6 +3,7 @@ import { FaSave } from "react-icons/fa";
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import "./My-profile.css";
+import photo from "./0f166c6cdf3643e7b8179432a1ddf709.png"
 
 const UserProfile = () => {
   // const history = useNavigate();
@@ -13,9 +14,9 @@ const UserProfile = () => {
   //   }
   // }, [history]);
   const [userData, setUserData] = useState({
-    firstName: "",
+    firstName: "asd",
     lastName: "",
-    email: "",
+    email: "asd",
     phone: "",
     photo: "",
   });
@@ -233,24 +234,30 @@ const UserProfile = () => {
     <div className="profile-background">
       <div className="container-profile-photo">
         <h2 className="profile-text">My Profile</h2>
+        <div>
+          <button className="upload-photo-button" onClick={handleButtonClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="70" viewBox="0 0 20 20" height="70" fill="none" className="svg-icon">
+              <g stroke-width="1.5" stroke-linecap="round" stroke="#de8a2a">
+                <circle r="7.5" cy="10" cx="10"></circle>
+                <path d="m9.99998 7.5v5"></path>
+                <path d="m7.5 9.99998h5"></path>
+              </g>
+            </svg>
+          </button>
+          <input className="choose-button" type="file" onChange={handleFileChange} style={{display: 'none'}} ref={fileInput} />
+        </div>
         <div className="background-picture-profile">
           <img
             className="profile-picture"
-            src={`data:image/jpeg;base64,${userData.photo}`}
+            // src={`data:image/jpeg;base64,${userData.photo}`}
+            src={photo}
             alt="profile picture"
           />
         </div>
-        <input className="choose-button" type="file" onChange={handleFileChange} style={{display: 'none'}} ref={fileInput} />
         
-        <button className="upload-photo-button" onClick={handleButtonClick}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="70" viewBox="0 0 20 20" height="60" fill="none" className="svg-icon">
-            <g stroke-width="1.5" stroke-linecap="round" stroke="#de8a2a">
-              <circle r="7.5" cy="10" cx="10"></circle>
-              <path d="m9.99998 7.5v5"></path>
-              <path d="m7.5 9.99998h5"></path>
-            </g>
-          </svg>
-        </button>
+        
+        
+        
 
         <div className="form-group-profile">
           <label htmlFor="email">Email:</label>
@@ -337,7 +344,77 @@ const UserProfile = () => {
             </div>
           </div>
         </div>
-        <div className="container-profile-recent"></div>
+        <div className="container-profile-recent">
+        <div className="form-group-profile">
+            <label htmlFor="firstName">First Name:</label>
+            <div className="profile-detail">
+              <input
+                type="firstname"
+                id="firstName"
+                value={editedFirstName || userData.firstName}
+                onChange={(e) => setEditedFirstName(e.target.value)}
+                readOnly={!editedFirstName}
+              />
+              {editedFirstName ? (
+                <button className="save-button" onClick={handleSaveFirstName}>
+                  <FaSave />
+                </button>
+              ) : (
+                <button className="edit-button" onClick={handleEditFirstName}>
+                  <svg className="edit-svgIcon" viewBox="0 0 512 512">
+                    <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
+                  </svg>
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="form-group-profile">
+            <label htmlFor="lastName">Last Name:</label>
+            <div className="profile-detail">
+              <input
+                type="lastname"
+                id="lastName"
+                value={editedLastName || userData.lastName}
+                onChange={(e) => setEditedLastName(e.target.value)}
+                readOnly={!editedLastName}
+              />
+              {editedLastName ? (
+                <button className="save-button" onClick={handleSaveLastName}>
+                  <FaSave />
+                </button>
+              ) : (
+                <button className="edit-button" onClick={handleEditLastName}>
+                  <svg className="edit-svgIcon" viewBox="0 0 512 512">
+                    <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
+                  </svg>
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="form-group-profile">
+            <label htmlFor="phoneNumber">Phone Number:</label>
+            <div className="profile-detail">
+              <input
+                type="tel"
+                id="phoneNumber"
+                value={editedPhone || userData.phone}
+                onChange={(e) => setEditedPhone(e.target.value)}
+                readOnly={!editedPhone}
+              />
+              {editedPhone ? (
+                <button className="save-button" onClick={handleSavePhone}>
+                  <FaSave />
+                </button>
+              ) : (
+                <button className="edit-button" onClick={handleEditPhone}>
+                  <svg className="edit-svgIcon" viewBox="0 0 512 512">
+                    <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
+                  </svg>
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
