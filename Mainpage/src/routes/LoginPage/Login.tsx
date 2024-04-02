b49991ce4c63648b64620472d65d4ccbfe509ae2
sutanuadhikary <sutanu.adhikary.8131@gmail.com>
2024-04-02 17:58:16 +0530

    login page background change

--- Mainpage/src/routes/LoginPage/Login.tsx
+++ Mainpage/src/routes/LoginPage/Login.tsx
@@ -5,6 +5,18 @@ import './Login.css'
 const LoginMain = () => {
   return (
     <div className='container-design'>
+      <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+      </ul>
       <div className='login-message'>
         <h1>  Hello there,  </h1>
         <h1> How you doing </h1>
