81b9522f0bd13254ad4f9791ef22604cb95751be
nisarganag <nisarga.nag.cse24@heritageit.edu.in>
2024-05-11 17:10:36 +0530

    minor changes

--- Mainpage/src/routes/MyProfile/profile_routes/recents.tsx
+++ Mainpage/src/routes/MyProfile/profile_routes/recents.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { BASE_URL } from "../../config/config";
 
 interface Alert {
@@ -9,28 +9,28 @@ interface Alert {
 function NotificationsDropdown() {
   const [alerts, setAlerts] = useState<Alert[]>([]);
 
-  useEffect(() => {
-    const fetchAlerts = async () => {
-      const userEmail = localStorage.getItem("emailId");
-      let res = await fetch(`${BASE_URL}/api/v1/auth/view?email=${userEmail}`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      const userData = await res.json();
-      const userId = userData.id;
-      res = await fetch(`${BASE_URL}/api/v1/notifications/all/${userId}`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      let data = await res.json();
-      data = data.filter((alert: { status: number }) => alert.status !== 1);
-      setAlerts(data);
-    };
-
-    fetchAlerts();
+  const fetchAlerts = useCallback(async () => {
+    const userEmail = localStorage.getItem("emailId");
+    let res = await fetch(`${BASE_URL}/api/v1/auth/view?email=${userEmail}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const userData = await res.json();
+    const userId = userData.id;
+    res = await fetch(`${BASE_URL}/api/v1/notifications/all/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    let data = await res.json();
+    data = data.filter((alert: { status: number }) => alert.status !== 1);
+    setAlerts(data);
   }, []);
+  
+  useEffect(() => {
+    fetchAlerts();
+  }, [fetchAlerts]);
 
   return (
     <div className="profile-recents">
@@ -45,6 +45,34 @@ function NotificationsDropdown() {
           ) : (
             <h1>No new message</h1>
           )}
+          <div>
+          <button
+              onClick={async () => {
+                  const userEmail = localStorage.getItem("emailId");
+                  const res = await fetch(`${BASE_URL}/api/v1/auth/view?email=${userEmail}`, {
+                    headers: {
+                      Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                  });
+                  const userData = await res.json();
+                  const userId = userData.id;
+                const response = await fetch(`${BASE_URL}/api/v1/notifications/updateNotificationStatus/${userId}`, {
+                  method: 'PUT', // or 'PUT'
+                  headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                  },
+                  // Include any necessary data in the body
+                });
+                if (!response.ok) {
+                  // Handle error
+                  console.error('Error:', response.status);
+                }
+                fetchAlerts();
+              }}
+            >
+              Mark all as read
+            </button>
+          </div>
         </div>
     </div>
   );
