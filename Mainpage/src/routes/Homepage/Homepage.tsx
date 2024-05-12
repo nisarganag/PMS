277eda554c73c850b7909caa84ef006a7b06188f
nisarganag <nisarga.nag.cse24@heritageit.edu.in>
2024-05-12 14:31:07 +0530

    minor changes

--- Mainpage/src/routes/Homepage/Homepage.tsx
+++ Mainpage/src/routes/Homepage/Homepage.tsx
@@ -3,12 +3,42 @@ import Searchbar from './Searchbar';
 import SearchResults from './SearchResults';
 import logo from './Screenshot_2024-03-20_202235.png';
 import darkLogo from './Screenshot_2024-03-20_202252.png'; // Replace with the path to your dark mode logo
+import { useCallback, useEffect, useState } from 'react';
+import { BASE_URL } from '../config/config';
 interface HomeProps {
   isDarkMode: boolean;
 }
 function Home({ isDarkMode }: HomeProps) {
+  const [data, setData] = useState<string | null>(null);
   const currentLogo = isDarkMode ? darkLogo : logo;
 
+  const recommendation = useCallback(async () => {
+    const userEmail = localStorage.getItem("emailId");
+    let res = await fetch(`${BASE_URL}/api/v1/auth/view?email=${userEmail}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const userData = await res.json();
+    const userId = userData.id;
+    res = await fetch(`${BASE_URL}/api/v1/publications/preprocessPublications/${userId}`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const publicationData = await res.text();
+    setData(publicationData);
+  }, []);
+
+  useEffect(() => {
+    recommendation();
+  }, [recommendation]);
+  const handleTitleClick = (title: string) => {
+    // Handle the click event here
+    console.log(`Title clicked: ${title}`);
+  };
+  const publications = data ? data.split('ID: ') : [];
   return (
     <div className='homePage'>
       <div className='search_bar'>
@@ -17,6 +47,21 @@ function Home({ isDarkMode }: HomeProps) {
         </div>
         <Searchbar />
         <SearchResults />
+        {publications.map((publication, index) => {
+        // Ignore the first empty string from the split
+        if (index === 0) return null;
+
+        // Extract the title from the publication string
+        const title = publication.split(', Title: ')[1];
+
+        return (
+          <div key={index}>
+      <a href="#" onClick={() => handleTitleClick(title)}>
+        <p>{title}</p>
+      </a>
+    </div>
+        );
+      })}
       </div>
     </div>
   );
